54ef6af2a5577427c5a5a749155fe5e9124c74db
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-20 17:16:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Driveless::Application.config.secret_token = '91190e4273aecab14de1d6881a19e1ef7310733f4650e06990d27edcee94c4d55a253792f017b5ac52c9a7c2d12b82cd08a9129a80d48e80dc04afeea557232e'
